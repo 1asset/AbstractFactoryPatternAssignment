18e133cb3fbfdb578b4ce560210c08bbaee12880
1asset <82859085+1asset@users.noreply.github.com>
2021-10-19 22:13:58 +0600

    Create ElectricGuitarFactory.java

---
+++ src/com/company/factory/ElectricGuitarFactory.java
@@ -0,0 +1,27 @@
+package com.company.factory;
+
+import com.company.guitar.ElectricGuitar;
+import com.company.guitar.Guitar;
+
+public class ElectricGuitarFactory implements GuitarAbstractFactory {
+
+    private String model;
+    private String brand;
+    private String pickups;
+    private double scaleLength;
+    private int price;
+
+    public ElectricGuitarFactory(String model, String brand, String pickups, double scaleLength, int price) {
+        this.model = model;
+        this.brand = brand;
+        this.pickups = pickups;
+        this.scaleLength = scaleLength;
+        this.price = price;
+    }
+
+    @Override
+    public Guitar createGuitar() {
+        return new ElectricGuitar(model,brand,pickups,scaleLength,price);
+    }
+
+}
