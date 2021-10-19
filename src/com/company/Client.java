1556567a6d285492d0d1acb6ceab6293a52bd249
1asset <82859085+1asset@users.noreply.github.com>
2021-10-19 22:11:14 +0600

    Create Client.java

---
+++ src/com/company/Client.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import com.company.factory.AcousticGuitarFactory;
+import com.company.factory.ClassicalGuitarFactory;
+import com.company.factory.ElectricGuitarFactory;
+import com.company.guitar.Guitar;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ElectricGuitarFactory JBM100Factory = new ElectricGuitarFactory("JBM100","Ibanez","DiMarzio Titan",25.5,3000);
+        ElectricGuitarFactory KellyFactory = new ElectricGuitarFactory("Kelly","Jackson","Seymour Duncan",25,2300);
+        ElectricGuitarFactory MH1000Factory = new ElectricGuitarFactory("MH1000","ESP","EMG",25,1700);
+
+        Guitar guitar1 = JBM100Factory.createGuitar();
+        Guitar guitar2 = JBM100Factory.createGuitar();
+        Guitar guitar3 = KellyFactory.createGuitar();
+        Guitar guitar4 = MH1000Factory.createGuitar();
+
+        System.out.println("guitar1 specs:");
+        System.out.println(     guitar1);
+        System.out.println("guitar2 specs:");
+        System.out.println(     guitar2);
+        System.out.println("guitar3 specs:");
+        System.out.println(     guitar3);
+        System.out.println("guitar4 specs:");
+        System.out.println(     guitar4);
+
+        ClassicalGuitarFactory BadGuitarFactory = new ClassicalGuitarFactory("garbage","trash","N/A",24,22);
+        Guitar guitar5 = BadGuitarFactory.createGuitar();
+        System.out.println("guitar5 specs:");
+        System.out.println(     guitar5);
+
+        AcousticGuitarFactory WorseGuitarFactory = new AcousticGuitarFactory("junk","rubbish","N/A",24.5,150);
+        Guitar guitar6 = WorseGuitarFactory.createGuitar();
+        System.out.println("guitar6 specs:");
+        System.out.println(     guitar6);
+    }
+}
